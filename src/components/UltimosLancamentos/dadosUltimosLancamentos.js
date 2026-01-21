9238983f49befe3048b3e21111a78c2307ed88d1
Fabricio <fabriciogarciacarvalho87@hotmail.com>
2026-01-21 18:34:25 -0300

    criando card de recomendação

--- src/components/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/components/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -1,6 +1,7 @@
-import livro from '../../images/livro.png'
+import Warcraft from '../../images/world-of-warcraft-dawn-of-the-aspects-part-i-9781416550693_hr.png'
+import Danganronpa from '../../images/dr0.png'
 
 export const livros = [
-    {id: 1, nome: "WOW", src: livro},
-    {id: 2, nome: "Danganronpa 0", src: livro}
+    {id: 1, nome: "WOW", src: Warcraft},
+    {id: 2, nome: "Danganronpa 0", src: Danganronpa}
 ]
--- src/components/UltimosLancamentos/index.js
+++ src/components/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from "./dadosUltimosLancamentos";
 import styled from "styled-components";
 import { Titulo } from "../Titulo";
+import livroRecomenda from "../../images/steel_ball_run.png"
+import CardRecomenda from "../CardRecomenda";
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -19,15 +21,27 @@ const NovosLivrosContainer = styled.div`
     cursor: pointer;
 `
 
+const LivroImagem = styled.img`
+    width: 300px; 
+    height: auto; 
+`
+
+
 function UltimosLancamentos() {
     return(
         <UltimosLancamentosContainer>
-            <Titulo>Últimos Lançamentos</Titulo>
+            <Titulo cor="#EB9B00" tamanhoFonte="36px">Últimos Lançamentos</Titulo>
             <NovosLivrosContainer>
             {livros.map( livro => (
-                <img src={livro.src}/>
+                <LivroImagem key={livro.id} src={livro.src} alt={livro.nome}/>
             ))}
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você goste de:"
+                subtitulo="Jojo's Bizarre Adventure Parte 7: Steel Ball Run"
+                descricao="a sétima parte da série mangá Jojo's bizarre adventure"
+                img={livroRecomenda}
+            />
         </UltimosLancamentosContainer>
     )
 }
